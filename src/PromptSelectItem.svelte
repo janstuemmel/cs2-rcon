1f15835d71bb077c5274df68ea58546a425d7b24
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-11-19 23:21:35 +0100

    better select bg

--- src/PromptSelectItem.svelte
+++ src/PromptSelectItem.svelte
@@ -4,7 +4,7 @@
   export let value: string = '';
   export let defaultValue: string = '';
   export let description: string;
-  let elem: HTMLDivElement;
+  let elem: HTMLButtonElement;
   
   export let scrollTo: (top: number) => any
 
@@ -13,7 +13,7 @@
   }
 </script>
 
-<div bind:this={elem} class="p-4 min-h-[50px] flex flex-col gap-1" class:dark:bg-nord-2={active} class:bg-nord-5={active}>
+<button bind:this={elem} class="p-4 min-h-[50px] flex flex-col gap-1 w-full" class:dark:bg-nord-0={active} class:bg-nord-5={active}>
   <div class="flex items-center gap-2">
     <div class="truncate text-sm font-mono text-nord-0 dark:text-nord-4 font-semibold tracking-wider">{name}</div>
     {#if value.length < 10 && value !== ''}
@@ -23,5 +23,5 @@
       <div class="truncate text-xs font-mono text-nord-3 dark:text-gray-400">(default: {defaultValue})</div>
     {/if}
   </div>
-  <div class="text-xs font-mono text-nord-3 dark:text-gray-400">{description || 'n/a'}</div>
-</div>
+  <div class="text-xs font-mono text-nord-3 dark:text-gray-400 text-left">{description || 'n/a'}</div>
+</button>
